7ef6c1ea1d289a149a4fd89b7a3b98f6fb17a063
Jacob Gadberry <jagadberry03@gmail.com>
2025-02-03 19:35:05 -0500

    trying to call api from another point

--- backend/test.php
+++ backend/test.php
@@ -1,6 +1,9 @@
 <?php
 header('Content-Type: text/plain');
 
-echo "Hello from PHP";
+$currTime = new DateTimeImmutable();
+$current_time_string = $current_time->format(DateTimeInterface::ATOM);
+
+echo "Hello from PHP {$current_time_string}";
 exit();
 ?>
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,11 @@
 // Just a test function to hit a fake PHP endpoint
 async function testPHP() {
-  document.getElementById("fetchData").addEventListener("click", function () {
-    fetch("backend/test.php")
-      .then((response) => response.text())
-      .then((data) => {
-        document.getElementById("paraText").innerText = data
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error)
-      })
-  })
+  fetch("backend/test.php")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("getAPI").innerText = data
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error)
+    })
 }
