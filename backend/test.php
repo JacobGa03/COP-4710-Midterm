918e0ffe29d0f1ffe2edc358fbb13a6fba8cc0d5
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-02-05 12:08:49 -0500

    Set up test.php fuction and also set up a Postman endpoint

--- backend/test.php
+++ backend/test.php
@@ -1,9 +1,17 @@
 <?php
-header('Content-Type: text/plain');
+require 'index.php'; // Need this so we have base functionalities to make API calls
 
-$currTime = new DateTimeImmutable();
-$current_time_string = $current_time->format(DateTimeInterface::ATOM);
-
-echo "Hello from PHP {$current_time_string}";
-exit();
-?>
+try{
+    // Get the time in EST
+    date_default_timezone_set('America/New_York'); // Set the timezone to EST
+    $current_time = new DateTimeImmutable(); // Get the current time
+    $current_time_string = $current_time->format(DateTimeInterface::ATOM); // Format the time
+    // Format a response in JSON
+    $response = [
+        'current_time' => "The current time in EST is $current_time_string"
+    ];
+    returnJson($response);
+}catch(mysqli_sql_exception $ex){
+    // Some server error
+    returnError(CODE_SERVER_ERROR, $ex->getMessage());
+}?>
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 // Just a test function to hit a fake PHP endpoint
 async function testPHP() {
-  fetch("backend/test.php")
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("getAPI").innerText = data
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error)
-    })
+  // fetch("backend/test.php")
+  //   .then((response) => response.text())
+  //   .then((data) => {
+  //     document.getElementById("testingAPI").innerText = data
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching data:", error)
+  //   })
+  fetch("backend/test.php").then((response) =>
+    response
+      .text()
+      .then((data) => (document.getElementById("testingAPI").innerText = data))
+  )
 }
 
 async function login() {}
