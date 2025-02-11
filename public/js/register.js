08ddb6d897c445c9b3b10d5212e105e7c14e0d5b
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-02-11 00:52:25 -0500

    Register finally styled consistently

--- public/js/register.js
+++ public/js/register.js
@@ -1,14 +1,85 @@
-async function register() {
-  // Grab elements from the page
-  let email = $("body").find("#enterEmail")
-  let password = $("body").find("#enterPassword")
-  let confirmPassword = $("body").find("#reEnterPassword")
-  let school = $("body").find("#enterSchool")
-  let userType = $("input[name='form-check']:checked")
+$(document).ready(function () {
+  // Validate email
+  const email = document.getElementById("enterEmail")
+  let emailError = true
+  email.addEventListener("blur", () => {
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/
+    let s = email.value
 
-  console.log(`${email} ${password} ${confirmPassword} ${school} ${userType}`)
+    // Valid email
+    if (regex.test(s)) {
+      $("#enterEmail").removeClass("is-invalid").addClass("is-valid")
+      $("#emailError").hide()
+      emailError = true
+    }
+    // Invalid email
+    else {
+      $("#enterEmail").removeClass("is-valid").addClass("is-invalid")
+      $("#emailError").show()
+      emailError = false
+    }
+  })
 
-  // Ensure some other fields are checked and report their validity
-  // If checks fail, don't register the user
-  // Register the user with the database
-}
+  $("#passwordError").hide()
+  let passwordError = true
+  $("#enterPassword").keyup(function () {
+    validatePassword()
+  })
+
+  // Validate Password
+  function validatePassword() {
+    let passwordValue = $("#enterPassword").val()
+    // Invalid password
+    if (passwordValue == "") {
+      $("#enterPassword").removeClass("is-valid").addClass("is-invalid")
+      // $("#passwordError").show()
+      passwordError = false
+      return false
+    }
+    // Valid password
+    $("#enterPassword").removeClass("is-invalid").addClass("is-valid")
+    // $("#passwordError").hide()
+    passwordError = true
+    return true
+  }
+
+  $("#confirmPasswordError").hide()
+  let confirmationPasswordError = true
+  $("#reEnterPassword").keyup(function () {
+    validateConfirmationPass()
+  })
+
+  // Validate confirmation password
+  function validateConfirmationPass() {
+    let confirmation = $("#reEnterPassword").val()
+
+    // Password and confirm password don't match
+    if (confirmation != $("#enterPassword").val()) {
+      $("#reEnterPassword").removeClass("is-valid").addClass("is-invalid")
+      $("#confirmPasswordError").show()
+      confirmationPasswordError = false
+      return false
+    }
+    $("#reEnterPassword").removeClass("is-invalid").addClass("is-valid")
+    $("#confirmPasswordError").hide()
+    confirmationPasswordError = true
+    return true
+  }
+
+  // Validate school
+
+  // Handle submit
+  $("#submitRegister").click(function () {
+    console.log("trying to submit")
+    validateConfirmationPass()
+    validatePassword()
+    email.dispatchEvent(new Event("blur"))
+
+    // Ensure that both email and password were entered correctly
+    if (passwordError && emailError && confirmationPasswordError) {
+      return true
+    } else {
+      return false
+    }
+  })
+})
