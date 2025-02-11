92dc65b1fc83f10eb08d233d47f21de67f7ba567
Jacob Gadberry <jagadberry03@gmail.com>
2025-02-10 19:18:34 -0500

    Got login form validation how I want

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,51 @@
-import { sha256 } from "./sha256.js"
+// Majority of code used from GFG
+// https://www.geeksforgeeks.org/form-validation-using-jquery/
+$(document).ready(function () {
+  // Validate Email
+  const email = document.getElementById("enterEmail")
+  let emailError = true
+  email.addEventListener("blur", () => {
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/
+    let s = email.value
+    if (regex.test(s)) {
+      email.classList.remove("is-invalid")
+      emailError = true
+    } else {
+      email.classList.add("is-invalid")
+      emailError = false
+    }
+  })
 
-async function login() {
-  // Grab the credentials
-  let email = $("body").find("#enterEmail")
-  let password = $("body").find("#enterPassword")
+  $("#passwordError").hide()
+  let passwordError = true
+  $("#enterPassword").keyup(function () {
+    validatePassword()
+  })
 
-  if (!validEntry(email.val(), password.val())) return
+  // Validate Password
+  function validatePassword() {
+    let passwordValue = $("#enterPassword").val()
+    if (passwordValue == "") {
+      $("#passwordError").show()
+      $("#passwordError").html("Enter Your Password")
+      $("#passwordError").css("color", "red")
+      passwordError = false
+      return false
+    }
+    $("#passwordError").hide()
+    passwordError = true
+    return true
+  }
+  // Submit button
+  $("#submitLogin").click(function () {
+    validatePassword()
+    email.dispatchEvent(new Event("blur"))
 
-  // TODO: Log user in
-}
-
-function validEntry(email, password) {
-  return email != "" && password != ""
-}
-
-window.login = login
+    // Ensure that both email and password were entered correctly
+    if (passwordError && emailError) {
+      return true
+    } else {
+      return false
+    }
+  })
+})
