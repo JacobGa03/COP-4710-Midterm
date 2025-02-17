cffb8ac749e5b584811715051096a3bcb0cb404f
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-02-17 00:03:04 -0500

    Tables actually get created each time. The DB will be erased and then created again

--- saveDB.sh
+++ saveDB.sh
@@ -11,6 +11,9 @@ wait_for_mysql() {
 # Initialize the database
 initialize_db() {
   echo "Initializing database..."
+  # Drops old tables 
+  mysql -h db -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e "DROP DATABASE IF EXISTS ${MYSQL_DATABASE}; CREATE DATABASE ${MYSQL_DATABASE};"
+  # Clean slate
   mysql -h db -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /var/lib/mysql/DB_Setup.sql
 }
 
@@ -21,7 +24,7 @@ backup_db() {
 }
 
 # Trap the SIGTERM signal to backup the database when the container stops
-trap backup_db SIGTERM
+trap 'echo "Received SIGTERM, backing up database..."; backup_db' SIGTERM
 
 # Wait for MySQL server to be ready
 wait_for_mysql
