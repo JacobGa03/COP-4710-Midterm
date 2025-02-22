ed265d793c74d5269f482a114bc820ee318e7ea6
Jacob Gadberry <111665889+JacobGa03@users.noreply.github.com>
2025-02-21 23:59:22 -0500

    Can hit enpoint from javascript

--- public/js/index.js
+++ public/js/index.js
@@ -8,28 +8,65 @@ async function testPHP() {
 // requestData is some JSON object containing the info the specific endpoint needs.
 // method specifics which verb you'd like to run, ie. "POST", "GET", "UPDATE", etc.
 async function callAPI(endpoint, requestData, method) {
-  let url = "localhost:8000/backend" + endpoint
+  let url = "http://localhost:8000/backend" + endpoint
 
-  let options = {
-    body: JSON.stringify(requestData),
-    method: method,
-  }
-  // Make the call to PHP
-  let response = await fetch(url, options)
-
-  // Decode the response
-  let body = JSON.parse(await response.response.text())
+  const response = await formatResponse(url, method, requestData)
+  // Call the api
+  const result = await response.json()
 
   // Determine if response was successful
   switch (response.status) {
     case 200:
       console.log("Successful request...")
-      return [response.status, body]
-    case 400:
+      return [response.status, result]
+    case 404:
       console.log("Error result not found...")
-      return [response.status, null]
+      return [response.status, {}]
     case 500:
       console.log("Internal server error...")
-      return [response.status, null]
+      return [response.status, {}]
+    default:
+      console.log("Unexpected Error...")
+      return [response.status, {}]
   }
+}
+
+// Format the request based on the HTTP method
+async function formatResponse(url, method, requestData) {
+  if (method == "GET") {
+    url = url + "?" + new URLSearchParams(requestData).toString()
+    return await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  } else {
+    return await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    })
+  }
+}
+
+function saveUserCookie(user) {
+  // Save time stamp
+  let timestamp = new Date()
+  timestamp.setTime(timestamp.getTime() + 20 * 60 * 1000)
+  // Save user data
+  let jsonUserData = JSON.stringify(user)
+  jsonUserData = encodeURIComponent(jsonUserData)
+
+  // Store the user data in cookie
+  document.cookie =
+    "user=" +
+    jsonUserData +
+    ";" +
+    "expires=" +
+    timestamp.toUTCString() +
+    ";" +
+    "path=/"
 }
--- public/js/login.js
+++ public/js/login.js
@@ -51,10 +51,44 @@ $(document).ready(function () {
 
     // Ensure that both email and password were entered correctly
     if (passwordError && emailError) {
-      window.history.pushState("", "", "dashboard.html")
-      // return true
+      // Grab the email and hash the password
+      let email = $("#enterEmail").val()
+      let password = new jsSHA("SHA-256", "TEXT", { encoding: "UTF8" })
+      password.update($("#enterPassword").val())
+      let hashedPassword = password.getHash("HEX")
+
+      // Call the login function and handle the response
+      login(email, hashedPassword)
+        .then(([code, result]) => {
+          if (code != 200) {
+            // Handle error
+            $("#enterEmail").removeClass("is-valid").addClass("is-invalid")
+            $("#enterPassword").removeClass("is-valid").addClass("is-invalid")
+            $("#passwordError").show()
+          } else {
+            // Save the user information into a cookie
+            saveUserCookie(result)
+            // Redirect to dashboard
+            window.location.replace("dashboard.html")
+            return true
+          }
+        })
+        .catch((error) => {
+          // Handle any errors that occurred during the API call
+          console.error("Error during login:", error)
+          $("#enterEmail").removeClass("is-valid").addClass("is-invalid")
+        })
+      return false
     } else {
       return false
     }
   })
 })
+
+async function login(email, password) {
+  return await callAPI(
+    "/login.php",
+    { email: email, password: password },
+    "GET"
+  )
+}
