1672d09bc8e8bd8e666126a9ccd2c5cd4fdad12e
NElder730 <noahelder3@gmail.com>
2025-02-27 14:55:57 -0500

    register

--- backend/register.php
+++ backend/register.php
@@ -5,7 +5,8 @@ $data = getRequestInfo();
 $email = $data['email'];
 $password = $data['password'];  
 $university = $data['university'];
-$stu_id = '';
+$role = $data['role'];
+//$stu_id = '';
 
 
 $conn = getDbConnection();
@@ -13,21 +14,42 @@ if($conn->connect_error){
     returnError(CODE_SERVER_ERROR, 'Could not connect to the database');
 }
 else{
-    $stmt = $conn->prepare("SELECT email FROM Students WHERE email =?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if($row = $result->fetch_assoc()){
-        returnError(CODE_CONFLICT, 'User already exists');
-    }
-    else{
-        $stmt = $conn->prepare("INSERT INTO Students (stu_id,email, password, university) VALUES(UUID(),?,?,?)");
-        $stmt->bind_param("sss",$email, $password, $university);
+    if($role == 'student'){
+        $stmt = $conn->prepare("SELECT email FROM Students WHERE email =?");
+        $stmt->bind_param("s", $email);
         $stmt->execute();
-        returnJson(['stu_id'=> $conn->insert_id, 'email' => $email, 'university' => $university]);
+        $result = $stmt->get_result();
+        if($row = $result->fetch_assoc()){
+            returnError(CODE_CONFLICT, 'User already exists');
+        }
+        else{
+            $stmt = $conn->prepare("INSERT INTO Students (stu_id,email, password, university) VALUES(UUID(),?,?,?)");
+            $stmt->bind_param("sss",$email, $password, $university);
+            $stmt->execute();
+            returnJson(['stu_id'=> $conn->insert_id, 'email' => $email, 'university' => $university]);
+        }
+        $stmt->close();
+        $conn->close();
     }
-    $stmt->close();
-    $conn->close();
+    
+    else
+    {
+        $stmt = $conn->prepare("SELECT email FROM Super_Admins WHERE email =?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($row = $result->fetch_assoc()){
+            returnError(CODE_CONFLICT, 'User already exists');
+        }
+        else{
+            $stmt = $conn->prepare("INSERT INTO Super_Admins (sa_id,email, password, university) VALUES(UUID(),?,?,?)");
+            $stmt->bind_param("sss",$email, $password, $university);
+            $stmt->execute();
+            returnJson(['sa_id'=> $conn->insert_id, 'email' => $email, 'university' => $university]);
+        }
+        $stmt->close();
+        $conn->close();
+    }
 }
 
 
