710d3f1c6f0fab6a3ae05bf2bbb3555c3e7b56a9
Julian Mendis <pasas.mendis@gmail.com>
2025-03-27 15:21:16 -0400

    updated findevent.php

--- backend/findEvent.php
+++ backend/findEvent.php
@@ -10,7 +10,7 @@ if($conn->connect_error){
     returnError(CODE_SERVER_ERROR, 'Could not connect to the database');
 }
 else{
-    $query = "SELECT E.e_id, E.name, E.description, E.category, E.location, E.contact_info 
+    $query = "SELECT E.e_id, E.name, E.description, E.category, E.location, E.contact_info, E.time 
               FROM Events E 
               LEFT JOIN Private_Event PE ON E.e_id = PE.e_id 
               LEFT JOIN RSO_Event RE ON E.e_id = RE.e_id 
@@ -34,13 +34,22 @@ else{
         $types .= "s";
     }
     if (!empty($data['associated_uni'])) {
-        $query .= " AND PE.associated_uni = ? OR RE.associated_uni = ?";
+        $query .= " AND (PE.associated_uni = ? OR RE.associated_uni = ?)";
         $params[] = $data['associated_uni'];
         $params[] = $data['associated_uni'];
         $types .= "ss";
     }
+    if (!empty($data['time'])) {
+        $query .= " AND E.time = ?";
+        $params[] = $data['time'];
+        $types .= "s";
+    }
 
     $stmt = $conn->prepare($query);
+    if ($stmt === false) {
+        returnError(CODE_SERVER_ERROR, 'Prepare failed: ' . htmlspecialchars($conn->error));
+    }
+
     if (!empty($params)) {
         $stmt->bind_param($types, ...$params);
     }
@@ -52,7 +61,7 @@ else{
             $searchResults .= ",";
         }
         $searchCount++;
-        $searchResults .= '{"e_id": "' . $row['e_id'] . '", "name": "' . $row['name'] . '", "description": "' . $row['description'] . '", "category": "' . $row['category'] . '", "location": "' . $row['location'] . '", "contact_info": "' . $row['contact_info'] . '"}';
+        $searchResults .= '{"e_id": "' . $row['e_id'] . '", "name": "' . $row['name'] . '", "description": "' . $row['description'] . '", "category": "' . $row['category'] . '", "location": "' . $row['location'] . '", "contact_info": "' . $row['contact_info'] . '", "time": "' . $row['time'] . '"}';
     }
     if($searchCount == 0){
         returnError(CODE_NOT_FOUND, 'No events found');
